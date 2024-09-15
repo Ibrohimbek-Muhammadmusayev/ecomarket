fd2f43f5a2fa3d497680473ce92ec28c11e8b663
Ibrohimbek-Muhammadmusayev-new <ibrohimbekmuhammadmusayev34@gmail.com>
2024-09-15 11:13:22 +0500

    yangilandi navbar

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -11,7 +11,8 @@ import {
 } from "@/components/ui/popover"
 import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar"
 import React, { useEffect, useState } from 'react';
-import { Modal, Select, Space, Switch } from 'antd';
+import { Drawer, Modal, Select, Space, Switch } from 'antd';
+import type { DrawerProps, RadioChangeEvent } from 'antd';
 
 const OPTIONS = ['Apples', 'Nails', 'Bananas', 'Helicopters'];
 
@@ -24,6 +25,21 @@ export default function Navbar(){
             setToken(token)
         }
     }, [])
+
+    const [open, setOpen] = useState(false);
+    const [placement, setPlacement] = useState<DrawerProps['placement']>('left');
+
+    const showDrawer = () => {
+        setOpen(true);
+    };
+
+    const onClose = () => {
+        setOpen(false);
+    };
+
+    const onChange = (e: RadioChangeEvent) => {
+        setPlacement(e.target.value);
+    };
 
     const [isModalOpen, setIsModalOpen] = useState(false);
 
@@ -39,14 +55,19 @@ export default function Navbar(){
         setIsModalOpen(false);
     };
 
+    const onCategory = ()=>{
+        onClose()
+        showModal()
+    }
+
     const filteredOptions = OPTIONS.filter((o) => !selectedItems.includes(o));
 
     return (
         <div className="w-full">
-            <div className="w-full fixed z-50 items-center bg-green-700">
-                <div className="max-w-[1440px] items-center px-[20px] text-white mx-auto h-[40px] bg-green-700">
-                    <div className="flex items-center justify-between py-1">
-                        <ul className="flex gap-[50px]">
+            <div className="w-full fixed z-50 items-center bg-green-400">
+                <div className="max-w-[1440px] flex justify-between items-center px-[20px] text-white mx-auto h-[40px] sm:border-b md:border-b bg-green-400">
+                    <div className="flex items-center justify-center sm:justify-between 2xl:justify-between lg:justify-between md:justify-between xl:justify-between">
+                        <ul className="flex gap-[50px] mobilhidden">
                             <Link href={'/'}>
                                 <li className="font-semibold">Home</li>
                             </Link>
@@ -70,10 +91,107 @@ export default function Navbar(){
                                 </PopoverContent>
                             </Popover>
                             <Link href={'/'}>
-                                <li className="font-semibold">Delivery area</li>
+                                <li className="font-semibold">Delivery_area</li>
                             </Link>
                         </ul>
-                        <div className="sm:hidden md:hidden lg:hidden xl:flex 2xl:flex flex gap-[40px]">
+                        <Drawer
+                            title="Pagination"
+                            placement={placement}
+                            closable={true}
+                            onClose={onClose}
+                            width={320}
+                            open={open}
+                            key={placement}
+                        >
+                            <ul className="flex flex-col pb-[20px] border-b gap-[10px]">
+                                <Link href={'/'}>
+                                    <div className="w-full h-[30px] bg-slate-400 rounded-[6px] p-1">
+                                        <li className="font-semibold text-green-300">Home</li>
+                                    </div>
+                                </Link>
+                                <Link href={'/'}>
+                                    <div className="w-full h-[30px] bg-slate-400 rounded-[6px] p-1">
+                                        <li className="font-semibold text-green-300">About</li>
+                                    </div>
+                                </Link>
+                                <div onClick={onCategory} className="w-full h-[30px] bg-slate-400 rounded-[6px] p-1">
+                                    <li className="font-semibold text-start text-green-300">Categoriya</li>
+                                </div>
+                                <Modal title="Basic Modal" open={isModalOpen} onOk={handleOk} onCancel={handleCancel}>
+                                    <div className="flex justify-center flex-wrap gap-[30px]">
+                                        <div className="w-[70px] rounded-sm h-[70px] bg-slate-500"></div>
+                                        <div className="w-[70px] rounded-sm h-[70px] bg-slate-500"></div>
+                                        <div className="w-[70px] rounded-sm h-[70px] bg-slate-500"></div>
+                                        <div className="w-[70px] rounded-sm h-[70px] bg-slate-500"></div>
+                                        <div className="w-[70px] rounded-sm h-[70px] bg-slate-500"></div>
+                                        <div className="w-[70px] rounded-sm h-[70px] bg-slate-500"></div>
+                                        <div className="w-[70px] rounded-sm h-[70px] bg-slate-500"></div>
+                                        <div className="w-[70px] rounded-sm h-[70px] bg-slate-500"></div>
+                                        <div className="w-[70px] rounded-sm h-[70px] bg-slate-500"></div>
+                                        <div className="w-[70px] rounded-sm h-[70px] bg-slate-500"></div>
+                                    </div>
+                                </Modal>
+                                <Link href={'/'}>
+                                    <div className="w-full h-[30px] bg-slate-400 rounded-[6px] p-1">
+                                        <li className="font-semibold text-green-300">Delivery_area</li>
+                                    </div>
+                                </Link>
+                            </ul>
+                            <ul className="flex mt-[20px] flex-col pb-[20px] border-b gap-[10px]">
+                                <Link href={'/'}>
+                                    <div className="w-full h-[30px] bg-slate-400 rounded-[6px]">
+                                        <li className="flex justify-between px-[5px] text-green-300 items-center font-semibold">
+                                            Card
+                                            <Image
+                                                className="fill-white text-white"
+                                                src={'/Shopping-Cart.gif'}
+                                                alt="category"
+                                                width={30}
+                                                height={30}
+                                            />
+                                        </li>
+                                    </div>
+                                </Link>
+                                <Link href={'/login'}>
+                                    <div className="w-full h-[30px] bg-slate-400 rounded-[6px] p-1">
+                                        <li className="font-semibold text-green-300">Login</li>
+                                    </div>
+                                </Link>
+                                <Link href={'/signup'}>
+                                    <div className="w-full h-[30px] bg-slate-400 rounded-[6px] p-1">
+                                        <li className="font-semibold text-green-300">Signup</li>
+                                    </div>
+                                </Link>
+                            </ul>
+                            <h1 className="mt-[20px] text-[20px] font-bold">Admin Data</h1>
+                            <div className="flex flex-col rounded-[12px] pr-[10px] justify-center gap-[20px] flex-wrap">
+                                <h1 className="text-18px] font-bold">+998905843504</h1>
+                                <h1 className="text-18px] font-bold">ibrohimbekmuhammadmusayev@gmail.com</h1>
+                                <div className="flex justify-center gap-[10px]">
+                                    <Button className="bg-green-400">
+                                        <Link href={'https://t.me/Ibrohimbek_enginer'}>
+                                            <Image
+                                                src={'./telegram.svg'}
+                                                alt="telegram"
+                                                width={30}
+                                                height={30}
+                                                />
+                                        </Link>
+                                    </Button>
+                                    <Button className="bg-green-400">
+                                        <Link href={'https://www.instagram.com/ibrokhm.l6/'}>
+                                            <Image
+                                                src={'./instagram.svg'}
+                                                alt="telegram"
+                                                width={30}
+                                                height={30}
+                                            />
+                                        </Link>
+                                    </Button>
+                                </div>
+                            </div>
+                        </Drawer>
+                        <div className="sm:hidden md:hidden mobilhidden minwidthmobilhidden lg:hidden xl:flex 2xl:flex flex gap-[40px]">
                             <h1 className="font-semibold">+998905843504</h1>
                             <h1 className="font-semibold">ibrohimbekmuhammadmusayev@gmail.com</h1>
                             <div className="flex gap-[10px]">
@@ -95,51 +213,11 @@ export default function Navbar(){
                                 </Link>
                             </div>
                         </div>
-                        <div className="flex gap-[20px]">
-                            <div className="w-[40px] h-[30px]">
-                                <Space direction="vertical">
-                                    <Switch checkedChildren="UZ" unCheckedChildren="EN" defaultChecked />
-                                    <Switch checkedChildren="1" unCheckedChildren="0" />
-                                    <Switch
-                                    checkedChildren={<CheckOutlined />}
-                                    unCheckedChildren={<CloseOutlined />}
-                                    defaultChecked
-                                    />
-                                </Space>
-                            </div>
-                            <div className="sm:block md:block lg:block xl:hidden 2xl:hidden">
-                                <Button onClick={showModal} className="bg-white">
-                                    <Image
-                                        src={'/box.png'}
-                                        alt="user"
-                                        width={30}
-                                        height={30}
-                                    />
-                                </Button>
-                                <Modal title="Admin data" open={isModalOpen} onOk={handleOk} onCancel={handleCancel}>
-                                    <div className="flex bg-slate-400 rounded-[12px] p-[10px] justify-center gap-[20px] flex-wrap">
-                                        <h1>+998905843504</h1>
-                                        <h1>ibrohimbekmuhammadmusayev@gmail.com</h1>
-                                        <div className="flex gap-[10px]">
-                                            <Link href={'https://t.me/Ibrohimbek_enginer'}>
-                                                <Image
-                                                    src={'./telegram.svg'}
-                                                    alt="telegram"
-                                                    width={30}
-                                                    height={30}
-                                                />
-                                            </Link>
-                                            <Link href={'https://www.instagram.com/ibrokhm.l6/'}>
-                                                <Image
-                                                    src={'./instagram.svg'}
-                                                    alt="telegram"
-                                                    width={30}
-                                                    height={30}
-                                                />
-                                            </Link>
-                                        </div>
-                                    </div>
-                                </Modal>
+                        <div className="sm:hidden">
+                            <div className="h-[40px] px-[5px] bg-green-400">
+                                <Link href={'/'}>
+                                    <h1 className="text-[30px] drop-shadow-lg font-bold text-green-700"><span className="text-lime-600">Eco</span> market</h1>
+                                </Link>
                             </div>
                         </div>
                     </div>
@@ -155,12 +233,22 @@ export default function Navbar(){
                             </Avatar>
                             : null
                         }
-                        <div className="w-[165px]">
+                        <div className="w-[165px] mobilhidden">
                             <Link href={'/'}>
                                 <h1 className="text-[30px] drop-shadow-lg font-bold text-green-700"><span className="text-lime-600">Eco</span> market</h1>
                             </Link>
                         </div>
                     </div>
+                    <button className="bg-white w-[60px] sm:hidden flex justify-center overflow-hidden rounded text-black" onClick={showDrawer}>
+                        <Image
+                            className="w-[30px] mx-[8px] h-[30px]"
+                            src={'/hamburger.png'}
+                            alt="pagination logo"
+                            width={30}
+                            height={30}
+                        />
+                    </button>
+                    {/* asdasdasdasdasdasd */}
                     <Select
                         mode="multiple"
                         placeholder="Inserted are removed"
@@ -198,14 +286,6 @@ export default function Navbar(){
                             </div>
                         </PopoverContent>
                     </Popover>
-                    <Button className="bg-white sm:flex md:hidden h-[50px] w-[50px] p-0">
-                        <Image
-                            src={'/hause.png'}
-                            alt="category"
-                            width={30}
-                            height={30}
-                        />
-                    </Button>
                     <ul className="sm:hidden md:flex lg:flex xl:flex 2xl:flex  flex gap-[20px]">
                         <Popover>
                             <PopoverTrigger>
